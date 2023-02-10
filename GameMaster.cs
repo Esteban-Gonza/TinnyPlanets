91cab83556926e20164ee63628ac37d48b19cbf7
EstebanG <juanesgs2002@gmail.com>
2023-02-10 17:57:00 -0500

    Added score

--- GameMaster.cs
+++ GameMaster.cs
@@ -2,16 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameMaster : MonoBehaviour{
     
     public GameObject restarPanel;
 
+    public TMP_Text score;
+    bool hasLost;
+
     void Start() {
         restarPanel.SetActive(false);
     }
 
+    void Update() {
+        
+        if(hasLost == false){
+            score.text = Time.time.ToString("F0");
+        }
+    }
+
     public void GameOver(){
+        hasLost = true;
         Invoke("Delay", 1.5f);
     }
 
